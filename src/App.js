45e447dd4f02f26ed030ba61ac5b2f6f0a5717f1
VadimKurachynski <kurachynski@gmail.com>
2022-10-17 00:21:35 +0300

    2d

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Technologies from './Tecnologies';
 import Footer from './Footer';
-import Header from"./Header";
 import Content from './content';
+import Header from './Header';
+
+
 
 function App() {
   return (
-   <div>
-    {/* <Header /> */}
-    <Content />
-    <Technologies />
-    <Footer />
+    <div>
+      <Header />
+      <Content />
+      <Technologies />
+      <Footer />
+
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-const Header=()=> {
-    return (
-     <div>
-     <a href="#">Home</a> -
-     <a href="#">News Feed</a> -
-     <a href="#">Messages</a> -
-      </div>
-    );
-  }
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a> -
+      <a href="#">News Feed</a> -
+      <a href="#">Messages</a> -
+    </div>
+  );
+}
 
-  export default Header
+export default Header
