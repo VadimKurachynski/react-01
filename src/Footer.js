0776f704f9b4bcb494dd60b005aca73677126c8e
VadimKurachynski <kurachynski@gmail.com>
2022-10-16 23:27:34 +0300

    4f

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css';
 const Footer=()=> {
     return (
      <div>
-      {/* <div className='fot'>Footer</div> */}
+      <div className='fot'>Footer</div>
       <div>Footer</div>
       <div>Footer</div> 
       </div>
