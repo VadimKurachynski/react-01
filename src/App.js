9ef93040e7b6ad5e8708a8ef27b19674f4f461f1
VadimKurachynski <kurachynski@gmail.com>
2022-10-17 11:52:16 +0300

    component header

--- src/App.js
+++ src/App.js
@@ -12,10 +12,26 @@ function App() {
       </header>
       <nav className="nav">
         <div>Profile</div>
-        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settins</div>
       </nav>
       <div className="content">
-        Main content
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU" />
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My post
+          <div>New post</div>
+        </div>
+        <div>post1</div>
+        <div>post2</div>
+
+
       </div>
 
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header=() => {
+    return (
+        <header className="header">
+        <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" />
+      </header>
+    );
+}
+
+export default Header;
