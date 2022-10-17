ffe9787e2aafd640061273f96c7b2eb2575876fc
VadimKurachynski <kurachynski@gmail.com>
2022-10-17 10:56:16 +0300

    3f

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 // import logo from './logo.svg';
-
+import "./App.css";
 
 
 
 
 function App() {
   return (
-    <div className="">
-     <header>
-      <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png"/>
-     </header>
-<nav>
-  <div>Profile</div>
-  <div>Profile</div>
-</nav>
-<div>
-  Main content
-</div>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" />
+      </header>
+      <nav className="nav">
+        <div>Profile</div>
+        <div>Profile</div>
+      </nav>
+      <div className="content">
+        Main content
+      </div>
 
 
     </div>
