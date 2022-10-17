eb32de6a42d1f35f65015d1e4cad4cd20d5676c5
VadimKurachynski <kurachynski@gmail.com>
2022-10-17 12:21:20 +0300

    img

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Profile from "./components/Profile";
 
 
 
@@ -11,24 +12,9 @@ function App() {
     <div className="app-wrapper">
       <Header />
       <Nav />
+      <Profile />
 
 
-      <div className="content">
-        <div>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU" />
-        </div>
-        <div>
-          ava+description
-        </div>
-        <div>
-          My post
-          <div>New post</div>
-        </div>
-        <div>post1</div>
-        <div>post2</div>
-
-
-      </div>
 
 
     </div>
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU" />
+      </div>
+      <div>
+        ava+description
+      </div>
+      <div>
+        My post
+        <div>New post</div>
+      </div>
+      <div>post1</div>
+      <div>post2</div>
+
+
+    </div>
+
+  );
+}
+export default Profile;
