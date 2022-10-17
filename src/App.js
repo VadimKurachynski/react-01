29406aea7fd99605d128bc0ec374606e1f4ee33e
VadimKurachynski <kurachynski@gmail.com>
2022-10-17 12:03:48 +0300

    comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 
 
@@ -7,16 +9,10 @@ import "./App.css";
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" />
-      </header>
-      <nav className="nav">
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>News</div>
-        <div>Music</div>
-        <div>Settins</div>
-      </nav>
+      <Header />
+      <Nav />
+
+
       <div className="content">
         <div>
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU" />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav=() => {
+    return (
+      <nav className="nav">
+      <div>Profile</div>
+      <div>Messages</div>
+      <div>News</div>
+      <div>Music</div>
+      <div>Settins</div>
+    </nav>
+    );
+}
+export default Nav;
