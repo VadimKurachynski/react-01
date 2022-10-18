ebebf81726ae14732729fc15301895e6e2198c93
VadimKurachynski <kurachynski@gmail.com>
2022-10-18 22:56:32 +0300

    header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav=() => {
     return (
       <nav className="nav">
-      <div><a>Profile</a></div>
+      <div className="item"><a>Profile</a></div>
       <div><a>Messages</a></div>
       <div><a>News</a></div>
       <div><a>Music</a></div>
