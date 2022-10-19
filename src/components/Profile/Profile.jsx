ae44dba2cb3c4ace36f22baadcda84798ffa2ba5
VadimKurachynski <kurachynski@gmail.com>
2022-10-19 22:34:38 +0300

    post4

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ console.log(s);
 const Profile = () => {
     return (
         <div className={s.content}>
-            <div>
+            <div className={s.cont_img}>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU" />
             </div>
             <div>
