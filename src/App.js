79b82110e474474eba8607d6f31d73b703e9fb93
VadimKurachynski <kurachynski@gmail.com>
2022-10-20 23:09:18 +0300

    fg

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
-
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Nav/>
+            <Dialogs/>
+            {/*<Profile />*/}
 
 
-
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
