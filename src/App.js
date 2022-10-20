70e3204aed67ab61bbea67453c2137acf7a077e0
VadimKurachynski <kurachynski@gmail.com>
2022-10-20 23:58:17 +0300

    router3

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Nav/>
@@ -19,9 +20,8 @@ function App() {
             </div>
 
 
-
-
         </div>
+        </BrowserRouter>
     );
 }
 
