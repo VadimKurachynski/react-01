d62503b6a7473c7942688645f25f522a10e5ecaa
VadimKurachynski <kurachynski@gmail.com>
2022-10-21 12:32:34 +0300

    message1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,39 @@
 import s from "./Dialogs.module.css";
 
-const Dialogs=(props)=>{
-    return(
+const Dialogs = (props) => {
+    return (
         <div className={s.content}>
-            <div>Dialog</div>
+
+            <div className={s.dialogs}>
+               <div className={s.dialogsitems}>
+                 <div className={s.itemname}>
+                     Vadim
+                 </div>
+                   <div className={s.itemname}>
+                       Dima
+                   </div>
+                   <div className={s.itemname}>
+                       Olia
+                   </div>
+                   <div className={s.itemname}>
+                       Sveta
+                   </div>
+                   <div className={s.itemname}>
+                       Nikita
+                   </div>
+
+               </div>
+
+
+            </div>
+
+
+
+
+            <div className={s.messages}>
+                Messages
+
+            </div>
         </div>
     );
 
