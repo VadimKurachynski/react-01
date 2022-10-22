1ef1228ce5439f73521efda7ec11fba622fe7ca7
VadimKurachynski <kurachynski@gmail.com>
2022-10-22 15:27:48 +0300

    profileinfo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,21 +1,14 @@
 import MyPosts from "./MyPosts/MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 console.log(s);
 
 const Profile = () => {
     return (
-        <div >
-            <div className={s.cont_img}>
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU"/>
-            </div>
-            <div>
-                ava+description
-            </div>
-
+        <div>
+            <ProfileInfo/>
             <MyPosts/>
-
         </div>
 
     );
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,21 @@
+import s from "./ProfileInfo.module.css";
+
+console.log(s);
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div className={s.cont_img}>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU"/>
+            </div>
+
+            <div>
+                ava+description
+            </div>
+
+        </div>
+
+    );
+}
+export default ProfileInfo;
