3e0e8e9138a7548d0d0e54b01423420a4dc975d0
VadimKurachynski <kurachynski@gmail.com>
2022-10-22 17:20:59 +0300

    postElement

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,28 +1,30 @@
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+
 console.log(s);
 
 
-let postsData=[
-    {id: 1, message: `Hi, I am react developer`,likesCount: 12},
-    {id: 2, message: "Hi, how are you?",likesCount: 10},
-    {id: 3, message: "it's my first post",likesCount: 21},
+let postsData = [
+    {id: 1, message: `Hi, I am react developer`, likesCount: 12},
+    {id: 2, message: "Hi, how are you?", likesCount: 10},
+    {id: 3, message: "it's my first post", likesCount: 21},
 ]
 
-let post=postsData.map(p=><Post message={p.message} like={p.likesCount}/>)
+let postsElements = postsData.map(p => <Post message={p.message} like={p.likesCount}/>)
 
 
 const MyPosts = () => {
 
     return (
         <div className={s.postBlock}>
-           <h3>My posts</h3>
+            <h3>My posts</h3>
             <div>
-               <div><textarea></textarea></div>
-                <div><button>Add post</button></div>
+                <div><textarea></textarea></div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
-            {post}
-
+            {postsElements}
         </div>
 
     );
