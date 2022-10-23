f0d7f4e891048d41bfbaf6394748b359f1c2cd83
VadimKurachynski <kurachynski@gmail.com>
2022-10-23 21:30:47 +0300

    state

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App(props) {
             <Nav/>
             <div className="app-wrapper-content">
                 <Routes>
-                    <Route path="/profile" element={<Profile posts={props.appState.posts}/>}/>
-                    <Route path="/dialogs/*" element = {<Dialogs dialogs={props.appState.dialogs} messages={props.appState.messages}/>}/>
+                    <Route path="/profile" element={<Profile posts={props.state.profilePage.posts}/>}/>
+                    <Route path="/dialogs/*" element = {<Dialogs dialogs={props.state.profilePage.dialogs} messages={props.state.messagesPage.messages}/>}/>
                     <Route path="/news" element= {<News/>}/>
                     <Route path="/music" element= {<Music/>}/>
                     <Route path="/settins" element= {<Settins/>}/>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,22 +1,28 @@
 let state = {
-    posts: [
-        {id: 1, message: `Hi, I am react developer`, likesCount: 12},
-        {id: 2, message: "Hi, how are you?", likesCount: 10},
-        {id: 3, message: "it's my first post", likesCount: 21},
-    ],
-    messages: [
-        {id: 1, message: `Hi, I am react developer`},
-        {id: 2, message: `O no, does your friends play soccer?`},
-        {id: 3, message: `Yes, i can play the gitare!`},
-    ],
-    dialogs: [
-        {id: 1, name: "Vadim"},
-        {id: 2, name: "Dima"},
-        {id: 3, name: "Marina"},
-        {id: 4, name: "Sveta"},
-        {id: 5, name: "Nikita"},
-        {id: 6, name: "Karina"},
-    ]
+    profilePage: {
+        posts: [
+            {id: 1, message: `Hi, I am react developer`, likesCount: 12},
+            {id: 2, message: "Hi, how are you?", likesCount: 10},
+            {id: 3, message: "it's my first post", likesCount: 21},
+        ],
+        dialogs: [
+            {id: 1, name: "Vadim"},
+            {id: 2, name: "Dima"},
+            {id: 3, name: "Marina"},
+            {id: 4, name: "Sveta"},
+            {id: 5, name: "Nikita"},
+            {id: 6, name: "Karina"},
+        ]
+    },
+    messagesPage: {
+        messages: [
+            {id: 1, message: `Hi, I am react developer`},
+            {id: 2, message: `O no, does your friends play soccer?`},
+            {id: 3, message: `Yes, i can play the gitare!`},
+        ],
+    }
+
+
 }
 
 export default state;
