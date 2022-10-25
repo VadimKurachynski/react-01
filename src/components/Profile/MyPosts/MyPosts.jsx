f36f28a660f2ac2a6fd7f4bd805af443c4ba1fe2
VadimKurachynski <kurachynski@gmail.com>
2022-10-25 15:12:22 +0300

    onPostChange

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,26 +6,27 @@ import {createRef} from "react";
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post message={p.message} like={p.likesCount} id={p.id}/>)
     // let addPost=()=>{console.log("нажата кнопка")};
-    let newPostElement=createRef();
+    let newPostElement = createRef();
 
-    let addPost=()=>{
+    let addPost = () => {
 
-        let text=newPostElement.current.value;
+        let text = newPostElement.current.value;
         props.addPost(text);
-         newPostElement.current.value="";
+        newPostElement.current.value = "";
     }
 
-    let onPostChange=()=>{
-
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        console.log(text);
     }
 
     return (
-        <div  className={s.postBlock} >
+        <div className={s.postBlock}>
             <h3>My posts</h3>
             <div>
                 {/*<div><textarea ref={newPostElement}></textarea></div>*/}
                 {/*<div><textarea ref={newPostElement} onChange={addPost} /></div>*/}
-                <div><textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} /></div>
+                <div><textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/></div>
                 <div>
                     <button onClick={addPost}>Add post</button>
                 </div>
