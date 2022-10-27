b9a700f3b0e4f823e7db41b2099ad56767da97e1
VadimKurachynski <kurachynski@gmail.com>
2022-10-27 10:33:57 +0300

    store1

--- src/redux/state.js
+++ src/redux/state.js
@@ -64,5 +64,40 @@ let rescan =()=>{}
 rescan=observer;
 }
 
+let store={
+    _state :{
+        profilePage: {
+            posts: [
+                {id: 1, message: `Hi, I am react developer`, likesCount: 12},
+                {id: 2, message: "Hi, how are you?", likesCount: 10},
+                {id: 3, message: "it's my first post", likesCount: 21},
+            ],
+            newPostText:"it-vadim"
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Vadim"},
+                {id: 2, name: "Dima"},
+                {id: 3, name: "Marina"},
+                {id: 4, name: "Sveta"},
+                {id: 5, name: "Nikita"},
+                {id: 6, name: "Karina"},
+            ],
+            messages: [
+                {id: 1, message: `Hi, I am react developer`},
+                {id: 2, message: `O no, does your friends play soccer?`},
+                {id: 3, message: `Yes, i can play the gitare!`},
+            ],
+            textMessage:"vadim"
+        }
+    }
+
+
+
+}
+
+
+
+
 
 export default state;
