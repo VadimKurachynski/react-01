9d0bf434e79401b343f676d9ad3a93a37fe88457
VadimKurachynski <kurachynski@gmail.com>
2022-10-28 22:42:05 +0300

    redu

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,4 +1,24 @@
-const dialogsReducer=(state,action)=>{
-    
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_POST_MESSSAGE = 'UPDATE-NEW-POST-MESSSAGE';
+
+
+const dialogsReducer = (state, action) => {
+
+    if (action.type === ADD_MESSAGE) {
+        let newPost = {
+            id: 7,
+            message: state.textMessage,
+        };
+        state.messages.push(newPost);
+        state.textMessage = "";
+
+
+    } else if (action.type === UPDATE_NEW_POST_MESSSAGE) {
+        state.textMessage = action.newText;
+
+
+    }
+
+
     return state;
 }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,4 +1,25 @@
-const profileReducer=(state,action)=>{
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
+const profileReducer = (state, action) => {
+
+    if (action.type === ADD_POST) {
+
+        let newPost = {
+            id: 7,
+            message: state.newPostText,
+            likesCount: 7
+        };
+        state.posts.push(newPost);
+        state.newPostText = "";
+
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+        state.newPostText = action.newText;
+
+    }
+    ;
+
 
     return state;
 }
