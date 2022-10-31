f91af04fcf094a0308a260a23d643a558ccdb278
VadimKurachynski <kurachynski@gmail.com>
2022-10-31 21:13:12 +0300

    43_3

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,19 +1,16 @@
-import s from "./MyPosts.module.css";
-import Post from "./Post/Post";
-import {createRef} from "react";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 
 const MyPostsContainer = (props) => {
 
     let addPost = () => {
-       props.dispatch(addPostActionCreator());
+        props.store.dispatch(addPostActionCreator());
     }
 
     let onPostChange = (text) => {
-         props.dispatch(updateNewPostTextActionCreator(text));
+        props.store.dispatch(updateNewPostTextActionCreator(text));
     }
-    return ( <MyPosts updateNewPostText={onPostChange} addPost={addPost}/> );
+    return (<MyPosts updateNewPostText={onPostChange} addPost={addPost} posts={props.posts}/>);
 
 
 }
