41ca985d851ba4a399f0a59076e37f1bdf2e562a
VadimKurachynski <kurachynski@gmail.com>
2022-11-06 20:00:12 +0300

    2user

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,33 @@
-let Users = (prors) => {
+let Users = (props) => {
+
     return (
-        <div>
+    <div>
 
-            props.users.map(u=> <div key={u.id}></div>
+            { props.users.map(u => <div key={u.id}>
+
+               <span>
+                   <div>
+                       <img/>
+                   </div>
+                   <div>
+                       <button>Follow</button>
+                   </div>
+               </span>
+               <span></span>
+
+
+            </div>) }
 
 
 
-        </div>
-    )
+
+    </div>)
+
+
+
+
+
+
+
 }
-
 export default Users;
