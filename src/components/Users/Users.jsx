bf5faadd4d13b1489c6d80a3a2f321b9d31fdd79
VadimKurachynski <kurachynski@gmail.com>
2022-11-07 18:58:07 +0300

    class3

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,38 @@
 import s from "./Users.module.css"
 import axios from "axios";
 // import userPhoto from "/src/assets/img/user.png";
-import  userPhoto from "../../assets/img/user.png";
+import userPhoto from "../../assets/img/user.png";
 import React from 'react';
 
 
-class Users extends React.Component{
+class Users extends React.Component {
 
- constructor(props) {
-     super(props);
-     console.log((this.props.users));
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items);
+            });
+        }
+    }
 
-      if(this.props.users.length===0) {
-          axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-              this.props.setUsers(response.data.items);
-          });
-      }
- }
+    render() {
+        return (
+            <div>
 
-
-render() {
-return(
-    <div>
-
-        { this.props.users.map(u => <div key={u.id}>
+                {this.props.users.map(u => <div key={u.id}>
                <span>
                    <div>
-                        <img src={u.photos.small!=null ? u.photos.small : userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
                    </div>
                    <div>
-                       {u.followed?<button onClick={()=>{this.props.unfollow(u.id)}}>follow</button>:<button onClick={()=>{this.props.follow(u.id)}}>unfollow</button>}
+                       {u.followed ? <button onClick={() => {
+                           this.props.unfollow(u.id)
+                       }}>follow</button> : <button onClick={() => {
+                           this.props.follow(u.id)
+                       }}>unfollow</button>}
                    </div>
                </span>
-            <span>
+                    <span>
                    <span>
                        <div>{u.name}</div>
                        <div>{u.status}</div>
@@ -43,17 +43,13 @@ return(
                    </span>
 
                </span>
-        </div>) }
+                </div>)}
 
+            </div>
 
-    </div>
-
-)
-
-
+        )
+    }
 }
-}
-
 
 
 export default Users;
