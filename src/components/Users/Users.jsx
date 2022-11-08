1d5664985f341fcd63f5fa9cdb2895c75661d71b
VadimKurachynski <kurachynski@gmail.com>
2022-11-08 23:05:10 +0300

    3

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,6 +5,11 @@ import React from "react";
 
 let Users = (props) => {
 
+    let pagesCount=Math.ceil(props.totalUsersCount/props.pageSize);
+    let pages=[];
+    for (let i=1;i<=pagesCount;i++){
+        pages.push(i);
+    }
 
     return <div>
         <div>
--- src/components/Users/UsersAPiComponent.jsx
+++ src/components/Users/UsersAPiComponent.jsx
@@ -22,18 +22,19 @@ class UsersAPIComponent extends React.Component {
                 this.props.setUsers(response.data.items);
             });
         }
+
+
     render() {
 
-        // let pagesCount=Math.ceil(this.props.totalUsersCount/this.props.pageSize);
-        // let pages=[];
-        // for (let i=1;i<=pagesCount;i++){
-        //     pages.push(i);
-        // }
 
-        return <Users totalUserCount={this.props.totalUsersCount}
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
         pageSize={this.props.pageSize}
                       currentPage={this.props.currentPage}
                       onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
 
         />
     }
