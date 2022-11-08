642ee8b3409a2e569213e4a554f3a2875fc405c4
VadimKurachynski <kurachynski@gmail.com>
2022-11-09 01:42:12 +0300

    preload4

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,7 +10,7 @@ import {
     unfollowAC
 } from "../../redux/users-reducer";
 import Users from "./Users";
-
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
 
@@ -35,7 +35,7 @@ class UsersContainer extends React.Component {
 
     render() {
         return <>
-            {/*{this.props.isFetching?<img src={preloader} />:null}*/}
+            {this.props.isFetching?<Preloader />:null}
             <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
