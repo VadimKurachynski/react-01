e3eb8e9ee21310ba788b549a9a6836a1b3a1efcd
VadimKurachynski <kurachynski@gmail.com>
2022-11-10 22:26:48 +0300

    prof2

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import Profile from "./Profile";
 
 class ProfileContainer extends React.Component {
