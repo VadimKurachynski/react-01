cf99db6bd3a03db62a9a24892122642117c53da6
VadimKurachynski <kurachynski@gmail.com>
2022-11-10 23:14:02 +0300

    setUser

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -35,12 +35,10 @@ const profileReducer = (state = initialiState, action) => {
     }
 }
 
-export const addPostActionCreator = () => {
-    return {type: ADD_POST}
-}
-export const updateNewPostTextActionCreator = (text) => {
-    return {type: UPDATE_NEW_POST_TEXT, newText: text}
-}
+export const addPostActionCreator = () => { return {type: ADD_POST}}
+export const updateNewPostTextActionCreator = (text) => { return {type: UPDATE_NEW_POST_TEXT, newText: text}}
+export const setUserProfile = () => { return {type: ADD_POST}}
+
 
 
 export default profileReducer;
