e201a431b930331aaf590348e4da18cdc286582c
VadimKurachynski <kurachynski@gmail.com>
2022-11-11 15:52:52 +0300

    usedId

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
             <div className="app-wrapper-content">
                 <Routes>
                     <Route path="/profile/:userId" element={<ProfileContainer />}/>
+                    <Route path="/profile" element={<ProfileContainer />}/>
                     <Route path="/dialogs/*" element = {<DialogsContainer  />}/>
                     <Route path="/news" element= {<News/>}/>
                     <Route path="/music" element= {<Music/>}/>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -17,6 +17,7 @@ export function withRouter(Children){
 class ProfileContainer extends React.Component {
 componentDidMount() {
 let userId=this.props.match.params.userId;
+if(!userId) {userId=2};
 
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
          this.props.setUserProfile(response.data);
