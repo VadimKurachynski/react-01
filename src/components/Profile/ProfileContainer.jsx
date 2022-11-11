7e6d9d6629161086b37d2eace00f9bc3f97325d0
VadimKurachynski <kurachynski@gmail.com>
2022-11-11 14:58:39 +0300

    userid

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -16,8 +16,9 @@ export function withRouter(Children){
 
 class ProfileContainer extends React.Component {
 componentDidMount() {
+let userId=this.props.match.params.userId;
 
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
          this.props.setUserProfile(response.data);
     });
 }
@@ -33,5 +34,7 @@ let mapStateToProps=(state)=>({
     profile:state.profilePage.profile,
 });
 
+let withUrlDataContainerComponent=withRouter(ProfileContainer);
 
-export default connect(mapStateToProps,{setUserProfile}) (ProfileContainer);
+
+export default connect(mapStateToProps,{setUserProfile}) (withUrlDataContainerComponent);
