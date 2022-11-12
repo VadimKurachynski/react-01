38e3b65437b5da34f24291faf6841de9dc766b82
VadimKurachynski <kurachynski@gmail.com>
2022-11-12 20:26:38 +0300

    usrsAPI

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,31 @@
 import axios from "axios";
 
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {"API-KEY": "813d89a2-1811-42ec-93f9-532e518123b5"},
+});
 
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+            {
+                withCredentials: true
+            }).then(response => {
+            return response.data;
+        });
+    }
+}
 
-const instance=axios.create({
-    baseURL:'https://social-network.samuraijs.com/api/1.0/',
-        withCredentials: true,
-    headers:{"API-KEY":"813d89a2-1811-42ec-93f9-532e518123b5" },
-    });
+export const getUsers = (currentPage = 1, pageSize = 10) => {
 
-
-
-export const getUsers=(currentPage=1,pageSize=10)=>{
-
-   return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,
         {
             withCredentials: true
-        }).then(response=>{
-            return response.data;
-       });
+        }).then(response => {
+        return response.data;
+    });
 }
 
 
