d842abc2666b7866f20d688deb86c1c262ef43dc
VadimKurachynski <kurachynski@gmail.com>
2022-11-14 16:24:54 +0300

    wer6

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,7 +5,6 @@ import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
 import { useParams } from 'react-router-dom';
 
-
 export function withRouter(Children){
     return(props)=>{
         const match  = {params: useParams()};
