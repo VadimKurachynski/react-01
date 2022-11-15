eaff8d579df76d58665199c5bc64508cdb703fa9
VadimKurachynski <kurachynski@gmail.com>
2022-11-15 22:07:47 +0300

    thunk4

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,10 +3,10 @@ import {usersAPI} from "../api/api";
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
-const SET_CURRENT_PAGE='SET_CURRENT_PAGE';
-const SET_TOTAL_USERS_COUNT='SET_TOTAL_USERS_COUNT';
-const TOGGLE_IS_FETCHING='TOGGLE_IS_FETCHING';
-const TOGGLE_IS_FOLLOWING_PROGRESS='TOGGLE_IS_FOLLOWING_PROGRESS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 let initialiState = {
     users: [
@@ -15,15 +15,15 @@ let initialiState = {
         // {id: 3,photoUrl:'https://bipbap.ru/wp-content/uploads/2019/05/86ae0b2400c92d333751c8d9a9ae68e4.png', followed:false, fullName: `Lena`, status: "I am job", location:{city:'Pinsk',country:'Belarus'}},
         // {id: 4,photoUrl:'https://bipbap.ru/wp-content/uploads/2019/05/86ae0b2400c92d333751c8d9a9ae68e4.png', followed:true, fullName: `Vitalij`, status: "I am ingener", location:{city:'Pruzhany',country:'Belarus'}},
     ],
-    pageSize:5,
-    totalUsersCount:0,
-    currentPage:1,
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
     isFetching: false,
-    followingInProgress:[2,3],
+    followingInProgress: [2, 3],
 };
 
 const usersReducer = (state = initialiState, action) => {
-console.log("usersReducer--->",state,action);
+    console.log("usersReducer--->", state, action);
 
     switch (action.type) {
         case FOLLOW:
@@ -49,22 +49,22 @@ console.log("usersReducer--->",state,action);
                 })
             }
 
-        case SET_USERS:{
-            return{...state,users: action.users}
+        case SET_USERS: {
+            return {...state, users: action.users}
         }
 
-        case SET_CURRENT_PAGE:{
-            return{...state,currentPage: action.currentPage }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
         }
-        case SET_TOTAL_USERS_COUNT:{
-            return{...state,totalUsersCount: action.totalUsersCount }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.totalUsersCount}
         }
-        case TOGGLE_IS_FETCHING:{
-            return{...state,isFetching: action.isFetching }
+        case TOGGLE_IS_FETCHING: {
+            return {...state, isFetching: action.isFetching}
         }
 
-        case TOGGLE_IS_FOLLOWING_PROGRESS:{
-            return{
+        case TOGGLE_IS_FOLLOWING_PROGRESS: {
+            return {
                 ...state,
                 followingInProgress: action.isFetching
                     ? [...state.followingInProgress, action.userId]
@@ -84,23 +84,27 @@ export const setUsers = (users) => ({type: SET_USERS, users})
 export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
 export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount})
 export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
-export const toggleFollowingProgress = (isFetching,userId) => ({type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching,userId})
+export const toggleFollowingProgress = (isFetching, userId) => ({
+    type: TOGGLE_IS_FOLLOWING_PROGRESS,
+    isFetching,
+    userId
+})
 export default usersReducer;
 
 
-export const getUsersThunkCreator=()=>{
-    return (dispatch)=>{
+export const getUsersThunkCreator = (currentPage, pageSize) => {
 
- dispatch(toggleIsFetching(true)) ;
+    return (dispatch) => {
 
-    usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-        .then(data => {
-            dispatch(toggleIsFetching(false));
-            dispatch(setUsers(data.items));
-            dispatch(setTotalUsersCount(data.totalCount));
-        });
+        dispatch(toggleIsFetching(true));
+
+        usersAPI.getUsers(currentPage, pageSize)
+            .then(data => {
+                dispatch(toggleIsFetching(false));
+                dispatch(setUsers(data.items));
+                dispatch(setTotalUsersCount(data.totalCount));
+            });
 
 
-
-}
+    }
 }
