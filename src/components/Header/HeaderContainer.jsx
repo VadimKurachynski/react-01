56b37cbb7e2b13f0d6494048244f5a79db2915e3
VadimKurachynski <kurachynski@gmail.com>
2022-11-17 00:29:33 +0300

    thunk

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import {getAuthUserData} from "../../redux/auth-reducer";
 import {connect} from "react-redux";
-import {autAPI} from "../../api/api";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
