2d79c74079a7e89524913d14e84bd9867c961742
VadimKurachynski <kurachynski@gmail.com>
2022-11-18 23:37:54 +0300

    state local

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 class ProfileStatus extends React.Component {
-
+state={
+    editMode: true
+}
 
 
     render() {
         return (
 
             <div>
-                <div><span>{this.props.status}</span></div>
-                <div><input value={this.props.status}/></div>
+                {!this.state.editMode &&
+                    <div><span>{this.props.status}</span></div>
+                }
+                {this.state.editMode &&
+                    <div><input value={this.props.status}/></div>
+                }
+
             </div>
         )
     }
