c217bfb3a64679452d8c8063b811c4aa3fafedb9
VadimKurachynski <kurachynski@gmail.com>
2022-11-22 21:58:34 +0300

    form

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,6 +4,20 @@ const Login = () => {
     return (
         <div>
             <h1>LOGIN</h1>
+            <form>
+                <div>
+                    <input placeholder={"Login"}/>
+
+                </div>
+                <div>
+                    <input placeholder={"Password"}/>
+
+                </div>
+                <div>
+                    <input type={"checkbox"}/>
+                </div>
+
+            </form>
         </div>
     );
 }
