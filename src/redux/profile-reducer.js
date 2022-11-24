472959b620fd7c7687d1b9a33eb52b74acba85a2
VadimKurachynski <kurachynski@gmail.com>
2022-11-24 16:43:36 +0300

    4log

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 import {profileAPI, usersAPI} from "../api/api";
 import profile from "../components/Profile/Profile";
 
-
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -77,6 +76,5 @@ export const updateStatus = (status) => (dispatch)=>{
 
     });
 }
-
 
 export default profileReducer;
