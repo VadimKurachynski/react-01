40badf177c86e79472c46a5664995d94b8096da7
VadimKurachynski <kurachynski@gmail.com>
2022-11-24 16:44:51 +0300

    6l

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -11,7 +11,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div className={s.cont_img}>
-                    {/*<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOILfvBu6_5wt4RB_1Mz5l4Uenm8yXvW0qiA&usqp=CAU"/>*/}
+
             </div>
 
             <div>
