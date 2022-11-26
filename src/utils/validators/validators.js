fe7d2b303ab6ae68a53bbd263752b13561f88f48
VadimKurachynski <kurachynski@gmail.com>
2022-11-27 00:12:52 +0300

    3log

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -1,5 +1,4 @@
 
-
 export const required1=value=>{
     if(value) return undefined;
     return "Field is required";
@@ -9,6 +8,4 @@ export const maxLengthCreator=(maxLength)=>(value)=>{
     if(value.length>maxLength) return `max length is ${maxLength} symbols`;
     return undefined;
 }
-
-
 
