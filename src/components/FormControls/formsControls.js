a51796bf2acaabde4e9e0d7f2b90d025abcb7bdb
VadimKurachynski <kurachynski@gmail.com>
2022-11-26 12:58:13 +0300

    error

--- src/components/FormControls/formsControls.js
+++ src/components/FormControls/formsControls.js
@@ -1,9 +1,12 @@
 import React from "react";
 import s from "./FormControls.module.css";
-export const Textarea=(props)=>{
+export const Textarea=({input,meta:{touched,error,warning},...props})=>{
     return (
-        <div className={`${s.formControl}+${s.error}`}>
-            <textarea {...props.input}/>
+        <div className={s.formControl+" "+s.error}>
+            <div>
+            <textarea {...input}{...props}/>
+        </div>
+            {touched && error && <span>"some error"</span>}
         </div>
     )
 }
