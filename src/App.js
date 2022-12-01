a3503eb0dfaa6056b2d7fb0b6f3760c68c0bb029
VadimKurachynski <kurachynski@gmail.com>
2022-12-01 09:29:58 +0300

    appclass

--- src/App.js
+++ src/App.js
@@ -10,27 +10,31 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/login";
-function App() {
+import {Component} from "react";
 
-    return (
-        <div className="app-wrapper">
-            <HeaderContainer/>
-            <Nav/>
-            <div className="app-wrapper-content">
-                <Routes>
-                    <Route path="/profile/:userId" element={<ProfileContainer />}/>
-                    <Route path="/profile" element={<ProfileContainer />}/>
-                    <Route path="/dialogs/*" element = {<DialogsContainer  />}/>
-                    <Route path="/news" element= {<News/>}/>
-                    <Route path="/music" element= {<Music/>}/>
-                    <Route path="/users" element= {<UsersContainer />}/>
-                    <Route path="/settins" element= {<Settins/>}/>
-                    <Route path="/login" element= {<Login />}/>
-                </Routes>
+class App extends Component {
+    render() {
+
+        return (
+            <div className="app-wrapper">
+                <HeaderContainer/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path="/profile/:userId" element={<ProfileContainer/>}/>
+                        <Route path="/profile" element={<ProfileContainer/>}/>
+                        <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/users" element={<UsersContainer/>}/>
+                        <Route path="/settins" element={<Settins/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
 
-    );
+        );
+    }
 }
 
 export default App;
