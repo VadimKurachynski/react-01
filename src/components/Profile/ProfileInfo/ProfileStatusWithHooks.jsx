366ffb418784c3537b45b258ad08d21890d56421
VadimKurachynski <kurachynski@gmail.com>
2022-12-02 18:39:05 +0300

    useState

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,18 +1,46 @@
 import React, {useState} from "react";
 
+
+
+
+
+
+
 const ProfileStatusWithHooks =(props)=>{
 
-    let stateWithSetState=useState(false);
-   let editMode=stateWithSetState[0];
-    let setEditMode=stateWithSetState[1];
+    //   let stateWithSetState=useState(false);
+    let[editMode,setEditMode]=useState(false);
+
+
+    const activateEditMode = () => {
+        setEditMode(true);
+        }
+
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+      //  props.updateStatus(this.state.status);
+    }
+
+    const onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+
+        });
+    }
+
+
+
+
+
     return (
             <div>
                 {!editMode &&
-                    <div><div>статус: </div><span >{props.status} </span></div>
+                    <div><div>статус: </div><span onDoubleClick={activateEditMode}>{props.status} </span></div>
                 }
                 {editMode &&
                     <div>
-                        <input  autoFocus={true} />
+                        <input  autoFocus={true} onBlur={deactivateEditMode}/>
 
                     </div>
                 }
