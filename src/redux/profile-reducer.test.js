2b2a72b70295ea35cc872d5beb673717cf0df98b
VadimKurachynski <kurachynski@gmail.com>
2022-12-04 16:41:56 +0300

    test2

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,10 +1,19 @@
-import profileReducer from "./profile-reducer";
+import profileReducer, {addPostActionCreator} from "./profile-reducer";
 import {render, screen} from "@testing-library/react";
 import App from "../App";
 
 
-test('renders learn react link', () => {
-    let newState=profileReducer({},{});
+test('new post', () => {
+    let action =addPostActionCreator("ati-baty")
+    let state = {
+        posts: [
+            {id: 1, message: `Hi, I am react developer`, likesCount: 12},
+            {id: 2, message: "Hi, how are you?", likesCount: 10},
+            {id: 3, message: "it's my first post", likesCount: 21},
+        ]
+
+    };
+    let newState=profileReducer({},{action});
 
 });
 
