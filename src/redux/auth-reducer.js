69cb525fccb41d2d52d5bca925e2faf14f9cb068
VadimKurachynski <kurachynski@gmail.com>
2022-12-05 18:25:19 +0300

    ref

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -4,16 +4,15 @@ import {stopSubmit} from "redux-form";
 const SET_USER_DATA = 'samurai-network/auth/SET_USER_DATA';
 
 
-
 let initialiState = {
-userId:null,
-   email:null,
-    login:null,
-    isAuth:false,
+    userId: null,
+    email: null,
+    login: null,
+    isAuth: false,
 };
 
 const authReducer = (state = initialiState, action) => {
-console.log("authReducer--->",state, action);
+    console.log("authReducer--->", state, action);
 
     switch (action.type) {
         case SET_USER_DATA:
@@ -21,46 +20,52 @@ console.log("authReducer--->",state, action);
                 ...state,
                 ...action.payload,
 
-                }
+            }
 
         default:
             return state;
     }
 }
 
-export const setAuthUserData = (userId,email,login, isAuth) => ({type: SET_USER_DATA, payload:
-        {userId,email,login,isAuth}})
+export const setAuthUserData = (userId, email, login, isAuth) => ({
+    type: SET_USER_DATA, payload:
+        {userId, email, login, isAuth}
+})
 
-export const getAuthUserData=()=>(dispatch)=>{
-    return autAPI.me()
-        .then(response => {
-        if (response.data.resultCode === 0) {
-            let {id, email, login} = response.data.data;
-            dispatch(setAuthUserData(id, email, login,true));
-        }
-    });
+// export const getAuthUserData=()=>(dispatch)=>{
+//     return autAPI.me()
+//         .then(response => {
+//         if (response.data.resultCode === 0) {
+//             let {id, email, login} = response.data.data;
+//             dispatch(setAuthUserData(id, email, login,true));
+//         }
+//     });
+// }
 
+export const getAuthUserData = () => async (dispatch) => {
+    let response = await autAPI.me();
+    if (response.data.resultCode === 0) {
+        let {id, email, login} = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 }
 
-export const login=(email,password,rememberMe)=>(dispatch)=>{
-
-    autAPI.login(email,password,rememberMe)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-dispatch(getAuthUserData())
-            } else{
-                let message=response.data.messages.length>0?response.data.messages[0]
-                    : "some error";
-                dispatch(stopSubmit("login",{_error: message} ));
-            }
-        });
+export const login = (email, password, rememberMe) => async (dispatch) => {
+    let response = await autAPI.login(email, password, rememberMe);
+    if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData())
+    } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0]
+            : "some error";
+        dispatch(stopSubmit("login", {_error: message}));
+    }
 }
-export const logout=()=>(dispatch)=>{
-    autAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(setAuthUserData(null, null, null,false));
-            }
-        });
+
+export const logout = () => async (dispatch) => {
+    let response = await autAPI.logout();
+    if (response.data.resultCode === 0) {
+        dispatch(setAuthUserData(null, null, null, false));
+    }
+
 }
 export default authReducer;
