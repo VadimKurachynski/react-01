b141e1a023800cdf9c3c94c7abf9e09589a40c4a
VadimKurachynski <kurachynski@gmail.com>
2022-12-07 10:39:49 +0300

    refr

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -6,49 +6,51 @@ import {Navigate, useParams} from 'react-router-dom';
 import {compose} from "redux";
 
 
-export function withRouter(Children){
-    return(props)=>{
-        const match  = {params: useParams()};
-        return <Children {...props}  match = {match}/>
+export function withRouter(Children) {
+    return (props) => {
+        const match = {params: useParams()};
+        return <Children {...props} match={match}/>
     }
 }
 
 class ProfileContainer extends React.Component {
-componentDidMount() {
-let userId=this.props.match.params.userId;
-if(!userId) {
-    userId= this.props.autorizedUserId;
-};
-this.props.getUserProfile(userId);
-this.props.getStatus(userId);
-}
 
-    componentDidUpdate() {
-        let userId=this.props.match.params.userId;
-        if(!userId) {
-            userId= this.props.autorizedUserId;
-        };
+    refreshProfile(){
+        let userId = this.props.match.params.userId;
+        if (!userId) {
+            userId = this.props.autorizedUserId;
+        }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
+
+    componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate() {
+        this.refreshProfile();
+    }
+
     render() {
 
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status}
+                     updateStatus={this.props.updateStatus}/>
         );
     }
 }
 
-let mapStateToProps=(state)=>({
-    profile:state.profilePage.profile,
-    status:state.profilePage.status,
+let mapStateToProps = (state) => ({
+    profile: state.profilePage.profile,
+    status: state.profilePage.status,
     autorizedUserId: state.auth.userId,
     isAuth: state.auth.isAuth,
 
 });
 
 export default compose(
-    connect(mapStateToProps,{getUserProfile,getStatus,updateStatus}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
 )(ProfileContainer)
 
