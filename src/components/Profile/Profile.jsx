7ccd389c62bf0ff0f91809085536df0269e5df46
VadimKurachynski <kurachynski@gmail.com>
2022-12-07 12:01:39 +0300

    uploid photo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = (props) => {
     return (
         <div>
 
-            <ProfileInfo isOwner={props.isOwner}      profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileInfo savePhoto={props.savePhoto} isOwner={props.isOwner}      profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
             <MyPostsContainer />
         </div>
 
