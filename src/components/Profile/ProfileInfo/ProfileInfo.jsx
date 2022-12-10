dc1dddf9956b0bc461b2a8ff2c2494958724fd9b
VadimKurachynski <kurachynski@gmail.com>
2022-12-10 16:27:13 +0300

    34

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -25,7 +25,6 @@ const ProfileInfo = (props) => {
                 <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
 
-
             <div className={s.descriptionBlock}>
             </div>
             <div>user Id: {props.profile.userId}</div>
@@ -33,37 +32,37 @@ const ProfileInfo = (props) => {
             {props.isOwner && <input type={"file"} onChange={mainPhotoSelected}/>}
             <div><img src={props.profile.photos.small}/></div>
 
-
-
             <div>
                 <b>Full name</b>:{props.profile.fullName}
             </div>
             <div>
                 <b>Loking for a job</b>:{props.profile.lokingForAJob ? "yes" : "no"}
             </div>
-            {props.profile.lokingForAJob&&
-            <div>
-                <b>My professional skills</b>:{props.profile.lokingForAJobDescription}
-            </div>
+            {props.profile.lokingForAJob &&
+                <div>
+                    <b>My professional skills</b>:{props.profile.lokingForAJobDescription}
+                </div>
             }
             <div>
                 <b>About Me</b>:{props.profile.about}
             </div>
             <div>
-                <b>Contacts</b>:{Object.keys(props.profile.contacts).map(key=>
-               <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]} />
-
+                <b>Contacts</b>:{Object.keys(props.profile.contacts).map(key =>
+                <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>
             )}
             </div>
-
-
-
         </div>
     );
 }
 
 
-const Contact=({contactTitle, contactValue})=> {
+const ProfileData = ({profile}) => {
+
+
+}
+
+
+const Contact = ({contactTitle, contactValue}) => {
     return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>
 
 }
