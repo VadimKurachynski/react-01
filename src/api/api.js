cb8f18e6740ad674eb07b257b8efbd4b15dbe94d
VadimKurachynski <kurachynski@gmail.com>
2022-12-16 20:36:39 +0300

    23

--- src/api/api.js
+++ src/api/api.js
@@ -9,63 +9,65 @@ const instance = axios.create({
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`
-           ).then(response => {
+        ).then(response => {
             return response.data;
         });
     },
-    unfollow(userId){
-    return instance.delete(`follow/${userId}`)
-    .then(response => {
-        return response.data.resultCode;
-
-    });
-},
-    follow(userId){
-        return instance.post(`follow/${userId}`,{})
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
             .then(response => {
                 return response.data.resultCode;
 
             });
     },
-    getProfile(userId){
+    follow(userId) {
+        return instance.post(`follow/${userId}`, {})
+            .then(response => {
+                return response.data.resultCode;
+
+            });
+    },
+    getProfile(userId) {
         return profileAPI.getProfile(userId);
     }
 }
 
 export const profileAPI = {
-    getProfile(userId){
+    getProfile(userId) {
         return instance.get(`profile/${userId}`);
     },
-    getStatus(userId){
+    getStatus(userId) {
         return instance.get(`profile/status/${userId}`);
     },
-    updateStatus(status){
-        return instance.put(`profile/status`,{status:status});
+    updateStatus(status) {
+        return instance.put(`profile/status`, {status: status});
     },
-   savePhoto(photoFile){
-        const formData=new FormData();
-        formData.append("image",photoFile);
-        return instance.put(`profile/photo`,formData,{headers:{'Content-Type':'multipart/form-data'}});
+    savePhoto(photoFile) {
+        const formData = new FormData();
+        formData.append("image", photoFile);
+        return instance.put(`profile/photo`, formData, {headers: {'Content-Type': 'multipart/form-data'}});
     },
 
-    saveProfile(profile){
-        return instance.put(`profile`,profile);
+    saveProfile(profile) {
+        return instance.put(`profile`, profile);
     },
 
 }
 
 export const autAPI = {
-    me(){
-        return  instance.get(`auth/me`); },
+    me() {
+        return instance.get(`auth/me`);
+    },
 
-    login(email,password,rememberMe=false,captcha=null){
-        return instance.post(`auth/login`,{email,password,rememberMe,captcha})},
+    login(email, password, rememberMe = false, captcha = null) {
+        return instance.post(`auth/login`, {email, password, rememberMe, captcha})
+    },
 
-    logout(){
-        return instance.delete(`auth/login`)}
+    logout() {
+        return instance.delete(`auth/login`)
+    }
 
 }
-
 
 export const securityAPI = {
     getCaptchaUrl() {
