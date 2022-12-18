3ae40beba15ac41b6130d645f799cc879eb2f746
VadimKurachynski <kurachynski@gmail.com>
2022-12-18 21:00:50 +0300

    24

--- src/components/FormControls/formsControls.js
+++ src/components/FormControls/formsControls.js
@@ -2,10 +2,10 @@ import React from "react";
 import s from "./FormControls.module.css";
 import {Field} from "redux-form";
 
-const FormControl=({input,meta:{touched,error},children})=>{
-    const hasError=touched&& error;
-    return(
-        <div className={s.formControl+" "+(hasError? s.error:"")}>
+const FormControl = ({input, meta: {touched, error}, children}) => {
+    const hasError = touched && error;
+    return (
+        <div className={s.formControl + " " + (hasError ? s.error : "")}>
             <div>
                 {children}
             </div>
@@ -24,14 +24,14 @@ const FormControl=({input,meta:{touched,error},children})=>{
 //     )
 // }
 
-export const Textarea=(props)=>{
-    const{input,meta,child,...restProps}=props;
-    return<FormControl {...props}><textarea{...input}{...restProps}/></FormControl>
+export const Textarea = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}><textarea{...input}{...restProps}/></FormControl>
 }
 
-export const Input=(props)=>{
-    const{input,meta,child,...restProps}=props;
-    return<FormControl {...props}><input{...input}{...restProps}/></FormControl>
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}><input{...input}{...restProps}/></FormControl>
 }
 
 
@@ -46,12 +46,12 @@ export const Input=(props)=>{
 //     )
 // }
 
-export const createField=(placeholder,name,validators, component, props={}, text="")=>(
+export const createField = (placeholder, name, validators, component, props = {}, text = "") => (
     <div>
-        <Field placeholder={placeholder}name={name}
+        <Field placeholder={placeholder} name={name}
                validate={validators}
                component={component}
-            {...props}
-            />{text}
+               {...props}
+        />{text}
     </div>
 )
