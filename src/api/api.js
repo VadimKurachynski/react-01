549aceb6d19e5cb3f307b74ecf8cc8f732f02af3
VadimKurachynski <kurachynski@gmail.com>
2022-12-20 10:15:28 +0300

    34

--- src/api/api.js
+++ src/api/api.js
@@ -39,9 +39,14 @@ export const profileAPI = {
     getStatus(userId) {
         return instance.get(`profile/status/${userId}`);
     },
+
+
     updateStatus(status) {
         return instance.put(`profile/status`, {status: status});
     },
+
+
+
     savePhoto(photoFile) {
         const formData = new FormData();
         formData.append("image", photoFile);
